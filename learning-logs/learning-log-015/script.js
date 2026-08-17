c5fdc4576b93c591551ea2b897a138bd4fb57366
ankitsaha0517 <ankitsaha0517@gmail.com>
2026-08-18 01:21:38 +0530

    feat: add Jason data add on js

---
+++ learning-logs/learning-log-015/script.js
@@ -0,0 +1,54 @@
+let data = [
+  {
+    name: "Roman Rouf Col.",
+    role: "Gym Expert",
+    isFriend: "Stranger",
+    rating: 4.5,
+    hours: 200,
+    months: "06",
+    status: "Online",
+    profileImage:
+      "https://i.pinimg.com/736x/ac/73/6d/ac736d1379e4b0ee390b35c0aea92419.jpg",
+    coverImage:
+      "https://i.pinimg.com/736x/96/c0/26/96c0262f8901320fded88cb085ed3f0c.jpg",
+  },
+  {
+    name: "Alex Morgan",
+    role: "Fitness Coach",
+    isFriend: "Stranger",
+    rating: 4.8,
+    hours: 320,
+    months: "08",
+    status: "Offline",
+    profileImage:
+      "https://i.pinimg.com/736x/5d/d2/85/5dd28576b5af0971341811d2c8954b92.jpg",
+    coverImage:
+      "https://i.pinimg.com/736x/90/27/84/9027841c926b495bf6e8343170307189.jpg",
+  },
+  {
+    name: "Daniel Smith",
+    role: "Personal Trainer",
+    isFriend: "Stranger",
+    rating: 4.6,
+    hours: 250,
+    months: "07",
+    status: "Online",
+    profileImage:
+      "https://i.pinimg.com/736x/28/9b/12/289b1254b2d51b27f4b0bad1b1cdda16.jpg",
+    coverImage:
+      "https://i.pinimg.com/1200x/2b/80/63/2b80632fc5c6d3ea1ef6469c8315e25d.jpg",
+  },
+  {
+    name: "James Wilson",
+    role: "Gym Expert",
+    isFriend: "Stranger",
+    rating: 4.9,
+    hours: 410,
+    months: "12",
+    status: "Offline",
+    profileImage:
+      "https://i.pinimg.com/736x/c3/83/df/c383df6f147ac8b320f9061f5118294d.jpg",
+    coverImage:
+      "https://i.pinimg.com/736x/99/2f/de/992fdecdc07942fc770f2fd6f45cfc89.jpg",
+  },
+];
